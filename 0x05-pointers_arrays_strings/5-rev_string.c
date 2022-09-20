02ad0158fd4f06727c4508d56d1fee1efbb621f2
timisi <afolayantoluwalope@gmail.com>
2022-09-20 10:42:13 -0700

    Another C program commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,21 +2,37 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character
- * @str: string
- *
- * Return: nothing
- */
+* rev_string - Reverses a string
+* @s: string to be reversed
+*
+* Return: nothing
+*/
 
-void puts2(char *str)
+void rev_string(char *s)
 {
-	int i = 0;
+	int i, tmp, len = _strlen(s);
 
-	while (*(str + i) != '\0')
+	for (i = 0; i < len / 2; i++)
 	{
-		if (i % 2 == 0)
-			putchar(*(str + i));
-		i++;
+		tmp = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
 	}
-	putchar(10);
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: string
+*
+* Return: the length of the given string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
 }
