63d507980db47308eb9532150588b807861161f6
Afolayan Tolulope <afolayantoluwalope@gmail.com>
2022-12-09 19:08:17 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,21 @@
-i
+#include "lists.h"
+
+/**
+ * print_dlistint - main functio
+ * dlistint_t - Pointer datatype
+ * @h: pointer
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
