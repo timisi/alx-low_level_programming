394428fed2a2859e5e8b961622e6e6f3f9ee4352
Afolayan Tolulope <afolayantoluwalope@gmail.com>
2022-12-09 19:13:48 +0100

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,21 @@
-troll
+#include "lists.h"
+
+/**
+ * dlistint_len - get number of nodes
+ * @h: the head of list
+ * Return: number pf nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	/* declarations */
+	int nodes = 0;
+
+	/* count nodes as we move through list */
+	while (h)
+	{
+		nodes += 1;
+		h = h->next;
+	}
+	return (nodes);
+}
