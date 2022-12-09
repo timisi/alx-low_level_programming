b7809c7df0a7de6948595b1e0a6b4c1ba5e2abb8
Afolayan Tolulope <afolayantoluwalope@gmail.com>
2022-12-09 19:20:16 +0100

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,20 @@
-tolu
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum all the ints in list
+ * @head: head of the list
+ * Return: sum or 0 if empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	/* declarations */
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
