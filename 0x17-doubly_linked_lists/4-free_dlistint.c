c4c057b0746df92e51eb1318434bb5a30f756b8d
Afolayan Tolulope <afolayantoluwalope@gmail.com>
2022-12-09 19:17:30 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,18 @@
-user
+#include "lists.h"
+
+/**
+ * free_dlistint - free a doubly linked list
+ * @head: the head of list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *phree;
+
+	while (head)
+	{
+		phree = head;
+		head = head->next;
+		free(phree);
+	}
+}
